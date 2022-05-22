fd8800b52ce793077b5ec9be6fd94cdb78a455d7
muhammadzamanshah <zamanluckybangash@gmail.com>
2022-05-22 18:25:28 +0500

    Cart_item_view_model modified

---
+++ lib/ui/screens/cart_section/cart_item/cart_item_view_model.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+import '../../../../core/models/cart_items.dart';
+
+class CartItemViewModel extends ChangeNotifier{
+
+  CartItem cartItem = CartItem(null,null,null,null,null);
+
+  void setCartItem(){
+  }
+}
