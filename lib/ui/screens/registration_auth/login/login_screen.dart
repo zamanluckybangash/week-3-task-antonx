2416bfef4c79a635acdc25f1d17201e4008134cf
jamshedkhan <jimykhan>
2022-05-26 10:34:51 +0500

    set validation in login page

--- lib/ui/screens/registration_auth/login/login_screen.dart
+++ lib/ui/screens/registration_auth/login/login_screen.dart
@@ -23,178 +23,181 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-
-    //final loginViewModel = Provider.of<LoginViewModel>(context);
-
-    return  Material(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            ClipPath(
-              child: Image.asset("assets/top.jpeg"),
-              clipper: MyClipper(),
-            ),
-            const SizedBox(height: 20,),
-            Stack(
-              overflow: Overflow.visible,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 90.0),
-                  child: Column(
-                      children: const [
-                        Text('Welcome back',
-                          style: TextStyle(
-                              color: darkGreenTextColor,
-                              fontWeight: FontWeight.w700,
-                              fontSize: 30.0
-                          ),
-                        ),
-                        SizedBox(height: 10,),
-                        Text('login to your account',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 15,
-                              color:greyColor
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 280.0,top: 2),
-                  child: SizedBox(
-                      height: 70,
-                      child: Image.asset("assets/leaf6.png",)
-                  ),
-                )
-              ],
-            ),
-            Form(
-              key: formKey,
-              child:  Column(
+    return  Consumer<LoginViewModel>(builder: (context, model, child){
+      return  Material(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ClipPath(
+                child: Image.asset("assets/top.jpeg"),
+                clipper: MyClipper(),
+              ),
+              const SizedBox(height: 20,),
+              Stack(
+                overflow: Overflow.visible,
                 children: [
-                  const SizedBox(height: 16,),
                   Padding(
-                    padding: const EdgeInsets.only(top: 40,left: 15,right: 15),
-                    child: InputTextFormField(
-                      hintText: "Full name",
-                      //errorText: loginViewModel.getName.error,
-                      icon: Icons.account_circle,
-                      iconColor: darkGreenTextColor,
-                      onChanged: (String value){
-                        //loginViewModel.changeName(value);
-                        Provider.of<LoginViewModel>(context,listen: false).changeName(value);
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15,left: 15,right: 15),
-                    child: InputTextFormField(
-                      hintText: 'password',
-                      //errorText: loginViewModel.getPassword.error,
-                      icon: Icons.lock,
-                      isPasswordActive: true,
-                      iconColor: darkGreenTextColor,
-                      onChanged: (String value){
-                        //loginViewModel.changePassword(value);
-                        Provider.of<LoginViewModel>(context,listen: false).changePassword(value);
-                      },
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Checkbox(
-                          activeColor: darkGreenTextColor,
-                          shape: const CircleBorder(),
-                          value: isChecked,
-                          onChanged: (selectedValue){
-                            setState(() {
-                              isChecked = selectedValue;
-                            });
-                          }
-                      ),
-                      const Text('Remember me',
-                        style: TextStyle(
-                            color: greyColor
-                        ),
-                      ),
-                      const SizedBox(width: 85,),
-                      const Text('forgote password?',
-                        style: TextStyle(
-                            color: darkGreenTextColor,
-                            fontWeight: FontWeight.w700
-                        ),
-                      )
-                    ],
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.only(left: 15,right: 15,top: 100),
-                      child:  RoundedRectangularButton(
-                          text: 'Login',
-                          buttonColor: darkGreenTextColor,
-                          onPressed: () {
-                            if(formKey.currentState!.validate()){
-                              return Navigator.push(context, MaterialPageRoute(builder: (context){
-                                return HomeScreen();
-                              }));
-                            }
-                            // print("Hello1");
-                            // if(!loginViewModel.isValid){
-                            //   print("Hello2");
-                            //   return null;
-                            // }
-                            // else{
-                            //   print("Hello3");
-                            //   Navigator.push(context, MaterialPageRoute(builder: (context){
-                            //     return HomeScreen();
-                            //   }));
-                            // }
-                          }
-                      )
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children:  [
-                      const Text("Don't have an account?",
-                        style: TextStyle(
-                            color: greyColor,
-                            fontWeight: FontWeight.w600
-                        ),
-                      ),
-                      TextButton(
-                          style: const ButtonStyle(
-                          ),
-                          onPressed: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context){
-                              return SignUpScreen();
-                            }));
-                            // if(!loginViewModel.isValid){
-                            //    Navigator.push(context, MaterialPageRoute(builder: (context){
-                            //     return SignUpScreen();
-                            //   }));
-                            // }
-                            // else{
-                            //    false;
-                            // }
-
-                          },
-                          child: const Text('Sign up',
+                    padding: const EdgeInsets.only(left: 90.0),
+                    child: Column(
+                        children: const [
+                          Text('Welcome back',
                             style: TextStyle(
-                                decoration: TextDecoration.underline,
                                 color: darkGreenTextColor,
-                                fontWeight: FontWeight.w600
+                                fontWeight: FontWeight.w700,
+                                fontSize: 30.0
                             ),
-                          )
-                      )
-                    ],
+                          ),
+                          SizedBox(height: 10,),
+                          Text('login to your account',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                fontSize: 15,
+                                color:greyColor
+                            ),
+                          ),
+                        ]
+                    ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 280.0,top: 2),
+                    child: SizedBox(
+                        height: 70,
+                        child: Image.asset("assets/leaf6.png",)
+                    ),
+                  )
                 ],
               ),
-            ),
-          ],
+              Form(
+                key: formKey,
+                child:  Column(
+                  children: [
+                    const SizedBox(height: 16,),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40,left: 15,right: 15),
+                      child: InputTextFormField(
+                        hintText: "Full name",
+                        //errorText: loginViewModel.getName.error,
+                        icon: Icons.account_circle,
+                        iconColor: darkGreenTextColor,
+                        validator: model.fullNameValidator,
+                        onChanged: (String value){
+                          //loginViewModel.changeName(value);
+                          Provider.of<LoginViewModel>(context,listen: false).changeName(value);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15,left: 15,right: 15),
+                      child: InputTextFormField(
+                        hintText: 'password',
+                        //errorText: loginViewModel.getPassword.error,
+                        icon: Icons.lock,
+                        isPasswordActive: true,
+                        validator: model.passwordNameValidator,
+                        iconColor: darkGreenTextColor,
+                        onChanged: (String value){
+                          //loginViewModel.changePassword(value);
+                          Provider.of<LoginViewModel>(context,listen: false).changePassword(value);
+                        },
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Checkbox(
+                            activeColor: darkGreenTextColor,
+                            shape: const CircleBorder(),
+                            value: isChecked,
+                            onChanged: (selectedValue){
+                              setState(() {
+                                isChecked = selectedValue;
+                              });
+                            }
+                        ),
+                        const Text('Remember me',
+                          style: TextStyle(
+                              color: greyColor
+                          ),
+                        ),
+                        const SizedBox(width: 85,),
+                        const Text('forgote password?',
+                          style: TextStyle(
+                              color: darkGreenTextColor,
+                              fontWeight: FontWeight.w700
+                          ),
+                        )
+                      ],
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(left: 15,right: 15,top: 100),
+                        child:  RoundedRectangularButton(
+                            text: 'Login',
+                            buttonColor: darkGreenTextColor,
+                            onPressed: () {
+                              if(formKey.currentState!.validate()){
+                                return Navigator.push(context, MaterialPageRoute(builder: (context){
+                                  return HomeScreen();
+                                }));
+                              }
+                              // print("Hello1");
+                              // if(!loginViewModel.isValid){
+                              //   print("Hello2");
+                              //   return null;
+                              // }
+                              // else{
+                              //   print("Hello3");
+                              //   Navigator.push(context, MaterialPageRoute(builder: (context){
+                              //     return HomeScreen();
+                              //   }));
+                              // }
+                            }
+                        )
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:  [
+                        const Text("Don't have an account?",
+                          style: TextStyle(
+                              color: greyColor,
+                              fontWeight: FontWeight.w600
+                          ),
+                        ),
+                        TextButton(
+                            style: const ButtonStyle(
+                            ),
+                            onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context){
+                                return SignUpScreen();
+                              }));
+                              // if(!loginViewModel.isValid){
+                              //    Navigator.push(context, MaterialPageRoute(builder: (context){
+                              //     return SignUpScreen();
+                              //   }));
+                              // }
+                              // else{
+                              //    false;
+                              // }
+
+                            },
+                            child: const Text('Sign up',
+                              style: TextStyle(
+                                  decoration: TextDecoration.underline,
+                                  color: darkGreenTextColor,
+                                  fontWeight: FontWeight.w600
+                              ),
+                            )
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+      );
+    }
     );
+    //final loginViewModel = Provider.of<LoginViewModel>(context);
   }
 }
